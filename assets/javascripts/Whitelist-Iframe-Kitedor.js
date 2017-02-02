a18bfe395cf6508c2ca28ca7bd61eb8fa6031561
AlexRabolin <alexrabolin@yahoo.fr>
2017-02-02 22:29:52 +0100

    Update Whitelist-Iframe-Kitedor.js

--- assets/javascripts/Whitelist-Iframe-Kitedor.js
+++ assets/javascripts/Whitelist-Iframe-Kitedor.js
@@ -1,6 +1,8 @@
 (function() {
   if (Discourse.dialect_deprecated) { return; }
 
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.meteoblue\.com\/fr\/meteo\/widget\/daily\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.meteoblue\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.windfinder\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.windguru\.cz\/.+/i);
+
 })();

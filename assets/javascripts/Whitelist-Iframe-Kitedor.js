41f5cb343952ad19c9b04f9bfe4da14075876570
AlexRabolin <alexrabolin@yahoo.fr>
2017-02-03 11:19:48 +0100

    Update Whitelist-Iframe-Kitedor.js

--- assets/javascripts/Whitelist-Iframe-Kitedor.js
+++ assets/javascripts/Whitelist-Iframe-Kitedor.js
@@ -6,12 +6,3 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.windguru\.cz\/.+/i);
 
 })();
-
-(function() {
-  if (Discourse.dialect_deprecated) { return; }
- 
-  Discourse.Markdown.whiteListTag('label', 'for', '*');
-  Discourse.Markdown.whiteListTag('i', 'class', '*');
-
-})();
-

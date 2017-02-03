ff4e37a1de8154157a08ddfd3ac14db3c3c40904
AlexRabolin <alexrabolin@yahoo.fr>
2017-02-03 09:58:26 +0100

    Update Whitelist-Iframe-Kitedor.js

--- assets/javascripts/Whitelist-Iframe-Kitedor.js
+++ assets/javascripts/Whitelist-Iframe-Kitedor.js
@@ -4,9 +4,9 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.meteoblue\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.windfinder\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.windguru\.cz\/.+/i);
-   Discourse.Markdown.whiteListTag('i', 'class');
+  Discourse.Markdown.whiteListTag('i', 'class');
   Discourse.Markdown.whiteListTag('label', 'for');
-  
+
 })();
 
 

cdf0ef8327e921ca85afeaecd9954fe91be221a5
AlexRabolin <alexrabolin@yahoo.fr>
2017-02-03 11:01:44 +0100

    Update Whitelist-Iframe-Kitedor.js

--- assets/javascripts/Whitelist-Iframe-Kitedor.js
+++ assets/javascripts/Whitelist-Iframe-Kitedor.js
@@ -7,4 +7,11 @@
 
 })();
 
+(function() {
+  if (Discourse.dialect_deprecated) { return; }
+ 
+  Discourse.Markdown.whiteListTag('label', 'for', '*');
+  Discourse.Markdown.whiteListTag('i', 'class', '*');
+
+})();
 
